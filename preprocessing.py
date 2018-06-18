154fc7507dd5516ae90736e34044eb5188816ab0
jczestochowska <justyna.czestochowska@nokia.com>
2018-06-18 16:48:18 +0200

    repeating signal length

--- preprocessing.py
+++ preprocessing.py
@@ -1,22 +1,41 @@
 import os
 import wave
+from copy import deepcopy
 
 import numpy as np
 
-
-def find_wav_length(filepath):
-    raw_signal = wave.open(filepath, 'rb')
-    signal = raw_signal.readframes(-1)
-    signal = np.frombuffer(signal, 'int16')
-    return len(signal)
+LONGEST_AUDIO_LENGTH = 396900
 
 
 def find_dataset_longest_wav(data_dir_path):
     the_longest = 0
     for wav_filename in os.listdir(data_dir_path):
-        wav_file_path = os.path.abspath(wav_filename)
+        wav_file_path = os.path.join(data_dir_path, wav_filename)
         new_length = find_wav_length(wav_file_path)
         if new_length > the_longest:
             the_longest = new_length
     return the_longest
 
+
+def find_wav_length(wav_filepath):
+    return len(get_raw_signal_from_file(wav_filepath))
+
+
+def get_raw_signal_from_file(wav_filepath):
+    raw_signal = wave.open(wav_filepath, 'rb')
+    initial_signal = raw_signal.readframes(-1)
+    return np.frombuffer(initial_signal, 'int16')
+
+
+def repeat_signal_length(initial_signal, expected_length=LONGEST_AUDIO_LENGTH):
+    signal = deepcopy(initial_signal)
+    if len(signal) != expected_length:
+        signal_length = len(initial_signal)
+        repetition_number = (expected_length // signal_length) - 1
+        repetition_modulo = expected_length % signal_length
+        while repetition_number:
+            signal = np.hstack((signal, initial_signal))
+            repetition_number -= 1
+        if repetition_modulo:
+            signal = np.hstack((signal, signal[:repetition_modulo]))
+    return signal
--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,27 +1,35 @@
-import wave
+import numpy as np
 from unittest import TestCase
 
-import numpy as np
+from preprocessing import find_wav_length, find_dataset_longest_wav, repeat_signal_length, get_raw_signal_from_file
 
-from preprocessing import find_wav_length, find_dataset_longest_wav
+TEST_DIR_PATH = '/home/jczestochowska/workspace/heartbeat_classification/tests/test_preprocessing_dir'
+TEST_FILEPATH = '/home/jczestochowska/workspace/heartbeat_classification/data/set_a/artifact__201012172012.wav'
+TEST_FILEPATH1 = '/home/jczestochowska/workspace/heartbeat_classification/data/set_b/extrastole__151_1306779785624_B.wav'
 
 
 class TestPreprocessing(TestCase):
-    def setUp(self):
-        self.test_dir_path = '/home/jczestochowska/workspace/heartbeat_classification/tests/test_preprocessing_dir'
-
     def test_find_wav_length(self):
-        test_filepath = '/home/jczestochowska/workspace/heartbeat_classification/data/set_a/artifact__201012172012.wav'
-        actual = find_wav_length(test_filepath)
+        actual = find_wav_length(TEST_FILEPATH)
         expected = 396900
         self.assertEqual(actual, expected)
 
     def test_find_wav_length_for_dir(self):
-        actual = find_dataset_longest_wav(self.test_dir_path)
+        actual = find_dataset_longest_wav(TEST_DIR_PATH)
         expected = 96640
         self.assertEqual(actual, expected)
 
+    def test_that_repeat_signal_doesnt_change_length(self):
+        expected = 3
+        actual = len(repeat_signal_length(np.array([1, 2, 3]), expected_length=expected))
+        self.assertEqual(actual, expected)
 
+    def test_that_repeat_signal_lengthens(self):
+        expected = 8
+        actual = len(repeat_signal_length(np.array([1, 2, 3, 4]), expected_length=expected))
+        self.assertEqual(actual, expected)
 
-
-
+    def test_get_raw_signal_from_file(self):
+        actual = len(get_raw_signal_from_file(TEST_FILEPATH1))
+        expected = 45233
+        self.assertEqual(actual, expected)
