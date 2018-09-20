8dca2b10a77afbbc267fbd66b94fff4e1db7648f
justyna <j.czestochowska@gmail.com>
2018-09-20 23:26:05 +0200

    adding changed notebooks, adding kaggle_dataset_overview file

---
+++ src/create_kaggle_dataset_overview_csv.py
@@ -0,0 +1,29 @@
+import csv
+import os
+import string
+from scipy.io import wavfile
+
+from config import PROJECT_ROOT_DIR
+from src.dataset_getters import get_set_name, \
+    get_kaggle_audio_dir_path, get_kaggle_label
+
+if __name__ == '__main__':
+    set_letters = list(string.ascii_lowercase[0:2])
+    csv_filename = 'kaggle_dataset_overview.csv'
+    saving_path = os.path.join(PROJECT_ROOT_DIR, 'data', 'processed', csv_filename)
+    with open(saving_path, 'w') as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow(['filename', 'recording_length', 'sampling_frequency', 'magnitude_bandwidth', 'label', 'set'])
+        for set_letter in set_letters:
+            audio_dir_path = get_kaggle_audio_dir_path(set_letter=set_letter)
+            filenames = os.listdir(audio_dir_path)
+            for filename in filenames:
+                file_path = os.path.join(audio_dir_path, filename)
+                label = get_kaggle_label(filename)
+                if label == 'extrahls':
+                    label = 'extrasystole'
+                sampling_frequency, signal = wavfile.read(file_path)
+                recording_length = round(len(signal) / sampling_frequency)
+                magnitude_bandwidth = max(signal) - min(signal)
+                writer.writerow([filename, recording_length, sampling_frequency, magnitude_bandwidth, label,
+                                 get_set_name(set_letter)])
