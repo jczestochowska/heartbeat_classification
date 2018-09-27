aa4e56ffb8e04c5b5b19032eccee7ddb582884d7
jczestochowska <justyna.czestochowska@nokia.com>
2018-09-27 15:30:44 +0200

    creating subdirectory for each label in preprocessing pipeline script, extending testcase

--- src/preprocessing_pipeline.py
+++ src/preprocessing_pipeline.py
@@ -7,7 +7,7 @@ from scipy.io import wavfile
 
 from config import PROJECT_ROOT_DIR
 from src.data_preparation import get_one_second_chunks, downsample_chunks, chunks_magnitude_normalization
-from src.dataset_getters import get_labels, get_label
+from src.dataset_getters import get_labels, get_label, LOGGER
 
 SOURCE_DIR = os.path.join(PROJECT_ROOT_DIR, 'data', 'raw', 'merged_datasets')
 DESTINATION_DIR = os.path.join(PROJECT_ROOT_DIR, 'data', 'processed', 'preprocessed')
@@ -21,16 +21,28 @@ def preprocess_file(filename, destination_dir=DESTINATION_DIR, labels=LABELS, ne
     chunks = get_one_second_chunks(sampling_rate=sampling_rate, signal=signal)
     dowsampled_chunks = downsample_chunks(chunks=chunks, new_sampling_rate=new_sampling_rate)
     normalized_chunks = chunks_magnitude_normalization(chunks=dowsampled_chunks)
+    label = get_label(labels=labels, audio_filename=filename)
     for chunk in normalized_chunks:
-        label = get_label(labels=labels, audio_filename=filename)
         if label is not None:
             new_filename = "{}_{}{}".format(label, uuid.uuid4().hex, ".wav")
-            new_file_path = os.path.join(destination_dir, new_filename)
+            new_file_path = resolve_destination_path(destination_dir, new_filename, label)
             wavfile.write(filename=new_file_path, rate=new_sampling_rate, data=np.array(chunk))
+
+
+def resolve_destination_path(destination_dir, new_filename, label):
+    label_dir = "normal"
+    if label == 0:
+        label_dir = "artifacts"
+    elif label == -1:
+        label_dir = "abnormal"
+    return os.path.join(destination_dir, label_dir, new_filename)
 
 
 if __name__ == '__main__':
     os.mkdir(DESTINATION_DIR)
+    os.mkdir(os.path.join(DESTINATION_DIR, "artifacts"))
+    os.mkdir(os.path.join(DESTINATION_DIR, "abnormal"))
+    os.mkdir(os.path.join(DESTINATION_DIR, "normal"))
     filenames = os.listdir(SOURCE_DIR)
     pool = Pool(5)
     pool.map(preprocess_file, filenames)
--- tests/test_preprocessing_pipeline.py
+++ tests/test_preprocessing_pipeline.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from unittest import TestCase
 
-from src.preprocessing_pipeline import preprocess_data
+from src.preprocessing_pipeline import preprocess_file
 from tests.test_signal_utils import TEST_DIR_PATH
 
 TEST_DESTINATION_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tmp")
@@ -14,6 +14,12 @@ class TestPreprocessingPipeline(TestCase):
             shutil.rmtree(TEST_DESTINATION_DIR)
 
     def test_preprocess_data(self):
-        preprocess_data(source_dir=TEST_DIR_PATH, destination_dir=TEST_DESTINATION_DIR)
-        self.assertGreater(len(os.listdir(TEST_DESTINATION_DIR)), len(os.listdir(TEST_DIR_PATH)))
+        os.mkdir(TEST_DESTINATION_DIR)
+        os.mkdir(os.path.join(TEST_DESTINATION_DIR, "artifacts"))
+        os.mkdir(os.path.join(TEST_DESTINATION_DIR, "abnormal"))
+        os.mkdir(os.path.join(TEST_DESTINATION_DIR, "normal"))
+        for filename in os.listdir(TEST_DIR_PATH):
+            preprocess_file(source_dir=TEST_DIR_PATH, destination_dir=TEST_DESTINATION_DIR, filename=filename)
+        self.assertEqual(len(os.listdir(TEST_DESTINATION_DIR)), 3)
+        self.assertEqual(len(os.listdir(os.path.join(TEST_DESTINATION_DIR, "normal"))), 35)
 
