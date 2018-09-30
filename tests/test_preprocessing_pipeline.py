583c13ec0c7e3619a506f0f93b99b50e75190b7f
justyna <j.czestochowska@gmail.com>
2018-09-30 13:43:23 +0200

    fixing unittests for preprocessing pipeline

--- tests/test_preprocessing_pipeline.py
+++ tests/test_preprocessing_pipeline.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from unittest import TestCase
 
-from src.preprocessing_pipeline import preprocess_file
+from src.preprocessing_pipeline import preprocess_physionet_file
 from tests.test_signal_utils import TEST_DIR_PATH
 
 TEST_DESTINATION_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tmp")
@@ -15,11 +15,6 @@ class TestPreprocessingPipeline(TestCase):
 
     def test_preprocess_data(self):
         os.mkdir(TEST_DESTINATION_DIR)
-        os.mkdir(os.path.join(TEST_DESTINATION_DIR, "artifacts"))
-        os.mkdir(os.path.join(TEST_DESTINATION_DIR, "abnormal"))
-        os.mkdir(os.path.join(TEST_DESTINATION_DIR, "normal"))
         for filename in os.listdir(TEST_DIR_PATH):
-            preprocess_file(source_dir=TEST_DIR_PATH, destination_dir=TEST_DESTINATION_DIR, filename=filename)
-        self.assertEqual(len(os.listdir(TEST_DESTINATION_DIR)), 3)
-        self.assertEqual(len(os.listdir(os.path.join(TEST_DESTINATION_DIR, "normal"))), 35)
-
+            preprocess_physionet_file(source_dir=TEST_DIR_PATH, destination_dir=TEST_DESTINATION_DIR, filename=filename)
+        self.assertEqual(len(os.listdir(TEST_DESTINATION_DIR)), 102)
