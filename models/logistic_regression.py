97d3a05f9571dd613ee39be54c572352f7328a3f
justyna <j.czestochowska@gmail.com>
2018-10-03 19:29:22 +0200

    correcting bug in script for serializing extracted features, adding script for logistic regression model

---
+++ models/logistic_regression.py
@@ -0,0 +1,61 @@
+import numpy as np
+import os
+import tensorflow as tf
+
+from config import PROJECT_ROOT_DIR
+
+TRAIN = os.path.join(PROJECT_ROOT_DIR, 'data', 'processed', 'preprocessed', 'physionet', 'serialized', 'train.npy')
+TEST = os.path.join(PROJECT_ROOT_DIR, 'data', 'processed', 'preprocessed', 'physionet', 'serialized', 'test.npy')
+TRAIN_LABELS = os.path.join(PROJECT_ROOT_DIR, 'data', 'processed', 'preprocessed', 'physionet', 'serialized',
+                            'train_labels.npy')
+TEST_LABELS = os.path.join(PROJECT_ROOT_DIR, 'data', 'processed', 'preprocessed', 'physionet', 'serialized',
+                           'test_labels.npy')
+
+
+def main(weights_shape, num_epochs, batch_size=200, training_step=0.2):
+    train_features, test_features, train_labels, test_labels = load_dataset()
+    random_normal_initializer = tf.initializers.random_normal()
+
+    weights = tf.get_variable('weights', shape=[train_features.shape[1], weights_shape], dtype=tf.float32,
+                              initializer=random_normal_initializer)
+    bias = tf.get_variable('bias', shape=weights_shape, dtype=tf.float32, initializer=random_normal_initializer)
+
+    features = tf.placeholder(shape=[None, train_features.shape[1]], dtype=tf.float32)
+    labels = tf.placeholder(shape=[None], dtype=tf.int64)
+
+    logits = tf.matmul(features, weights) + bias
+
+    cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+    optimizer = tf.train.GradientDescentOptimizer(training_step).minimize(cross_entropy)
+
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
+
+    for epoch in range(num_epochs):
+        batch_features, batch_labels = next_batch(batch_size, train_features, train_labels)
+        sess.run(optimizer, feed_dict={features: batch_features, labels: batch_labels})
+
+        correct_prediction = tf.equal(tf.argmax(logits, 1), labels)
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        print('-------------------------------------------------------------')
+        print("Epoch: {}".format(epoch))
+        print("training accuracy: ", sess.run(accuracy, feed_dict={features: train_features, labels: train_labels}))
+        print("testing accuracy: ", sess.run(accuracy, feed_dict={features: test_features, labels: test_labels}))
+        print('-------------------------------------------------------------')
+
+
+def load_dataset():
+    return np.load(TRAIN), np.load(TEST), np.load(TRAIN_LABELS), np.load(TEST_LABELS)
+
+
+def next_batch(num, data, labels):
+    idx = np.arange(0, len(data))
+    np.random.shuffle(idx)
+    idx = idx[:num]
+    data_shuffle = data[idx]
+    labels_shuffle = labels[idx]
+    return data_shuffle, labels_shuffle
+
+
+if __name__ == '__main__':
+    main(weights_shape=10, num_epochs=500)
--- src/serialize_extracted_features.py
+++ src/serialize_extracted_features.py
@@ -3,7 +3,8 @@ import os
 from multiprocessing.pool import Pool
 
 from config import PROJECT_ROOT_DIR
-from src.dataset_getters import get_numeric_labels_for_filenames
+from src.dataset_getters import get_label_from_filename_processed, \
+    map_physionet_label_to_number
 from src.feature_extraction import get_mfcc
 
 PHYSIONET_DIR = os.path.join(PROJECT_ROOT_DIR, 'data', 'processed', 'preprocessed', 'physionet')
@@ -12,22 +13,29 @@ TEST_DIR = os.path.join(PHYSIONET_DIR, 'test')
 DESTINATION_DIR = os.path.join(PHYSIONET_DIR, 'serialized')
 
 
-def serialize_features(filename, data_directory, output_filename):
-    features_npy = os.path.join(DESTINATION_DIR, output_filename + '.npy')
-    labels_npy = os.path.join(DESTINATION_DIR, output_filename + '_labels' + '.npy')
-    labels = np.array(get_numeric_labels_for_filenames(filename))
+def serialize_features(filename, data_directory):
+    label = np.array(map_physionet_label_to_number(get_label_from_filename_processed(filename)))
     filepath = os.path.join(data_directory, filename)
     features = np.array(get_mfcc(filepath))
     flatten_features = np.array(np.ravel(features))
-    np.save(features_npy, flatten_features)
-    np.save(labels_npy, labels)
+    return flatten_features, label
 
 
 if __name__ == '__main__':
+    train_npy = os.path.join(DESTINATION_DIR, 'train.npy')
+    test_npy = os.path.join(DESTINATION_DIR, 'test.npy')
+    train_labels_npy = os.path.join(DESTINATION_DIR, 'train_labels' + '.npy')
+    test_labels_npy = os.path.join(DESTINATION_DIR, 'test_labels' + '.npy')
     train_filenames = os.listdir(TRAINING_DIR)
     test_filenames = os.listdir(TEST_DIR)
-    train_filenames_with_output_names = [[filename] + [TRAINING_DIR, 'train'] for filename in train_filenames]
-    test_filenames_with_output_names = [[filename] + [TEST_DIR, 'test'] for filename in test_filenames]
-    pool = Pool(5)
-    pool.starmap(serialize_features, train_filenames_with_output_names)
-    pool.starmap(serialize_features, test_filenames_with_output_names)
+    train_filenames_with_output_names = [[filename] + [TRAINING_DIR] for filename in train_filenames]
+    test_filenames_with_output_names = [[filename] + [TEST_DIR] for filename in test_filenames]
+    pool = Pool(4)
+    train = pool.starmap(serialize_features, train_filenames_with_output_names)
+    test = pool.starmap(serialize_features, test_filenames_with_output_names)
+    train_features, train_labels = zip(*train)
+    test_features, test_labels = zip(*test)
+    np.save(train_npy, train_features)
+    np.save(train_labels_npy, train_labels)
+    np.save(test_npy, test_features)
+    np.save(test_labels_npy, test_labels)
