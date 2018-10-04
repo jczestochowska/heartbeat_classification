a9d3e29e3519b40d212616d248a36617b6210a8f
jczestochowska <justyna.czestochowska@nokia.com>
2018-10-04 14:13:25 +0200

    changed dimensions in model to fit one dimensional labels

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -16,9 +16,9 @@ def main(num_epochs, batch_size=200, training_step=0.2):
     train_features, test_features, train_labels, test_labels = load_dataset()
     random_normal_initializer = tf.initializers.random_normal()
 
-    weights = tf.get_variable('weights', shape=[train_features.shape[1], 2], dtype=tf.float32,
+    weights = tf.get_variable('weights', shape=[train_features.shape[1], 1], dtype=tf.float32,
                               initializer=random_normal_initializer)
-    bias = tf.get_variable('bias', shape=2, dtype=tf.float32, initializer=random_normal_initializer)
+    bias = tf.get_variable('bias', shape=[1], dtype=tf.float32, initializer=random_normal_initializer)
 
     features = tf.placeholder(shape=[None, train_features.shape[1]], dtype=tf.float32)
     labels = tf.placeholder(shape=[None], dtype=tf.int64)
