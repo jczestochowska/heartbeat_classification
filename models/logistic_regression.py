681224c1c1b0170f74e73f1c3b8147289164a3f8
justyna <j.czestochowska@gmail.com>
2018-10-07 16:42:11 +0200

    adding tensorboard visualizations

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import shutil
 import tensorflow as tf
 
 from config import PROJECT_ROOT_DIR
@@ -10,6 +11,7 @@ TRAIN_LABELS = os.path.join(PROJECT_ROOT_DIR, 'data', 'processed', 'preprocessed
                             'train_labels.npy')
 TEST_LABELS = os.path.join(PROJECT_ROOT_DIR, 'data', 'processed', 'preprocessed', 'physionet', 'serialized',
                            'test_labels.npy')
+SUMMARIES_DIR = os.path.join(PROJECT_ROOT_DIR, 'models', 'tensorboard_summaries')
 
 
 def main(num_epochs, batch_size=20, training_step=0.1):
@@ -23,27 +25,53 @@ def main(num_epochs, batch_size=20, training_step=0.1):
     features, labels = iterator.get_next()
     labels = tf.cast(labels, tf.int64)
 
-    weights = tf.Variable(tf.random_normal([train_features.shape[1], 2], dtype=tf.float64))
-    bias = tf.Variable(tf.random_normal([2], dtype=tf.float64))
+    weights = tf.Variable(tf.zeros([train_features.shape[1], 2], dtype=tf.float64))
+    bias = tf.Variable(tf.zeros([2], dtype=tf.float64))
 
     logits = tf.matmul(features, weights) + bias
 
-    cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-    optimizer = tf.train.GradientDescentOptimizer(training_step).minimize(cross_entropy)
+    with tf.name_scope('cross_entropy'):
+        with tf.name_scope('total'):
+            cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+    tf.summary.scalar('cross_entropy', cross_entropy)
+
+    with tf.name_scope('optimization'):
+        optimizer = tf.train.GradientDescentOptimizer(training_step).minimize(cross_entropy)
 
     sess.run(tf.global_variables_initializer())
 
+    with tf.name_scope('accuracy'):
+        with tf.name_scope('correct_prediction'):
+            correct_prediction = tf.equal(tf.argmax(logits, 1), labels)
+        with tf.name_scope('accuracy'):
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
+        with tf.name_scope('precision'):
+            precision, precision_update = tf.metrics.precision(labels=labels,
+                                                               predictions=tf.argmax(logits, 1),
+                                                               name="precision")
+        with tf.name_scope('recall'):
+            recall, recall_update = tf.metrics.recall(labels=labels,
+                                                      predictions=tf.argmax(logits, 1),
+                                                      name="recall")
+    tf.summary.scalar('accuracy', accuracy)
+    tf.summary.scalar('precision', precision)
+    tf.summary.scalar('recall', recall)
+
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter(SUMMARIES_DIR + '/train',
+                                         sess.graph)
+    test_writer = tf.summary.FileWriter(SUMMARIES_DIR + '/test')
+
+    sess.run(tf.local_variables_initializer())
+
     for epoch in range(num_epochs):
         _, loss_value = sess.run([optimizer, cross_entropy])
-        correct_prediction = tf.equal(tf.argmax(logits, 1), labels)
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
-        precision, precision_update = tf.metrics.precision(labels=labels,
-                                                           predictions=tf.argmax(logits, 1),
-                                                           name="precision")
-        recall, recall_update = tf.metrics.recall(labels=labels,
-                                                  predictions=tf.argmax(logits, 1),
-                                                  name="recall")
-        sess.run(tf.local_variables_initializer())
+        if epoch % 10 == 0:
+            train_summary = sess.run(merged)
+            train_writer.add_summary(train_summary, epoch)
+        test_summary = sess.run(merged, feed_dict={features: test_features, labels: test_labels})
+        test_writer.add_summary(test_summary, epoch)
 
         print('-------------------------------------------------------------')
         print("Epoch: {}".format(epoch))
@@ -76,5 +104,14 @@ def get_balanced_dataset(train_features, train_labels):
     return dataset.shuffle(buffer_size=train_features.shape[0])
 
 
+def delete_tensorboard_summaries():
+    train_summary = os.path.join(SUMMARIES_DIR, 'train')
+    test_summary = os.path.join(SUMMARIES_DIR, 'test')
+    if os.path.exists(train_summary) and os.path.exists(test_summary):
+        shutil.rmtree(train_summary)
+        shutil.rmtree(test_summary)
+
+
 if __name__ == '__main__':
-    main(num_epochs=200)
+    delete_tensorboard_summaries()
+    main(num_epochs=5000)
