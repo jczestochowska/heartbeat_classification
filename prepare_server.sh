74d514cb28f297c7dc4d4fb2ee793c78846216c2
jczestochowska <justyna.czestochowska@nokia.com>
2018-10-23 14:19:23 +0200

    making prepare_server executable, adding -y to ubuntu commands

--- prepare_server.sh
+++ prepare_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt-get update
-apt-get install python-pip
+apt-get update -y
+apt-get install -y python-pip
 pip install virtualenv
 mkdir ~/.virtualenvs
 pip install virtualenvwrapper
@@ -15,7 +15,5 @@ echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.profile
 source ~/.bashrc
 source ~/.profile
 
-git clone https://github.com/jczestochowska/heartbeat_classification.git
-cd heartbeat_classification
 mkvirtualenv heartbeat -r requirements.txt --python=python3.5
 
